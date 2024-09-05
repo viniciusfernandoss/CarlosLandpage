5addf141fdd21a28b0696ab834b450cfa43aa08f
viniciusfernandoss <vinicius.senac.ads@gmail.com>
2024-09-05 17:59:32 -0300

    trabalho 05/09/2024

---
+++ README.md
@@ -0,0 +1,1 @@
+Figma: https://www.figma.com/design/WzvCucPLZPB7ppMpLUGRoq/Projeto-Carlos-Santos?node-id=0-1
---
+++ js/reveal.js
@@ -0,0 +1,10 @@
+// window.revelara = ScrollReveal({reset:true})
+
+// // Destaques
+
+// revelara.reveal('.animation-test', {
+//     duration: 2000,
+//     distance: '90px'
+// })
+
+// ScrollReveal().reveal('.animation-test');
