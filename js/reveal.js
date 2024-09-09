7e76480d48a92a1876f3e0cf94a302339b0fa473
viniciusfernandoss <vinicius.senac.ads@gmail.com>
2024-09-09 17:50:54 -0300

    trabalho 09/09/2024 p3

--- js/reveal.js
+++ js/reveal.js
@@ -8,22 +8,26 @@ window.revelar = ScrollReveal({reset:true})
 //Sobre
 revelar.reveal('.animacao-txt-sobre', {
     duration: 1500,
-    distance: '120px'
+    distance: '120px',
+    reset: false
 })
 
 //Destaque/Cursos
 revelar.reveal('.animacao-card1', {
     duration: 1000,
     distance: '120px',
-    delay: 200
+    delay: 200,
+    reset: false
 })
 revelar.reveal('.animacao-card2', {
     duration: 1000,
     distance: '120px',
-    delay: 400
+    delay: 400,
+    reset: false
 })
 revelar.reveal('.animacao-card3', {
     duration: 1000,
     distance: '120px',
-    delay: 600
+    delay: 600,
+    reset: false
 })
